ae5c732a696d51edd17a2e3cd383082004012e5c
Lautaro <lautaro.yosbere@gmail.com>
2022-09-14 11:37:08 -0300

    configuracion inicial

---
+++ src/main/java/com/tiendavideojuegos/challenge_tienda_videojuegos/ChallengeTiendaVideojuegosApplication.java
@@ -0,0 +1,13 @@
+package com.tiendavideojuegos.challenge_tienda_videojuegos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeTiendaVideojuegosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeTiendaVideojuegosApplication.class, args);
+	}
+
+}
